14dc0ae50493f69d322cbf0e1919b8e771baee36
PratikshaDeshmukh0904 <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-05-08 22:28:36 +0530

    Print Welcome 10 times file uploaded

---
+++ Print-Welcome10times.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i;
+    //for(i=0;i<10; printf("Welcome \n", i++));
+    for( i = 1; i <=10 ; )
+    {
+        printf("Hello \n");
+        i++;
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
