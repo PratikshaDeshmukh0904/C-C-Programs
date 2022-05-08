9e56f73fd71a04ae994b32d7de60409a80e8e598
PratikshaDeshmukh0904 <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-05-08 22:30:29 +0530

    Square of number uploaded

---
+++ Square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,s,c;
+printf("\n Enter a Number:");
+scanf("\n %d",&a);
+s=a*a;
+c=a*a*a;
+printf("\n Square is:%d",s);
+printf("\n Cube is:%d",c);
+getch();
+}
