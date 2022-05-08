de2780c803905de31afe0cdc5f3e0e5e0aaf0ffc
PratikshaDeshmukh0904 <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-05-08 22:43:23 +0530

    Positive-Negative Number

---
+++ positivenegativeno.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\n Enter a number:");
+scanf("\n%d",&a);
+if(a>0)
+printf("%d is positive Number",a);
+else
+printf("%d is negative Number",a);
+}
