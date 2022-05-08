8adc262177b0534f0938b0e447191e94cf35d051
PratikshaDeshmukh0904 <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-05-08 22:29:38 +0530

    Print Welcome using while loop

---
+++ PrintHelloUsingWhileLoop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+printf("How many times you want to print Hello World");
+scanf("%d",&n);
+
+while(i<n)
+{
+printf("Hello World \n");
+i++;
+}
+printf("End");
+return 0;
+}
