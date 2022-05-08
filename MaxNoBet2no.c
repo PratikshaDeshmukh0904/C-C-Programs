7be2c74f9211690d66af43230ec791117d6aa5b5
PratikshaDeshmukh0904 <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-05-08 22:42:28 +0530

    Maximum No between 2 no

---
+++ MaxNoBet2no.c
@@ -0,0 +1,16 @@
+//#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b;
+    printf("Enter First Number \n");
+    scanf("\n %d",&a);
+    printf("Enter Second Number \n");
+    scanf("\n %d",&b);
+    if(a>b)
+        printf("%d is maximum no",a);
+    else
+        printf("%d is maximum no",b);
+    return 0;
+
+}
