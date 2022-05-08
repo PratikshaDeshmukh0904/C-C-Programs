4b8c9f61b9f6e8939aae29950b9516d69827092a
PratikshaDeshmukh0904 <103728005+PratikshaDeshmukh0904@users.noreply.github.com>
2022-05-08 22:33:40 +0530

    Addition of number is uploaded

---
+++ Addition.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("\n Enter Two Number:");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("Addition is:%d",c);
+c=a-b;
+printf("\nSubstraction is:%d",c);
+c=a*b;
+printf("\n Multiplication is: %d",c);
+c=a/b;
+printf("\n Division is:%d",c);
+getch();
+}
